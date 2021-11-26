3188608fe19bc2f84f69d39083fd421138418660
CheloCr <xchell_cruz@hotmail.com>
2021-11-26 11:30:43 -0600

    login y signup

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,3 +3,10 @@
 exports.home = async (req, res) => {
 	res.render("home")
 }
+
+exports.signup = async (req, res) => {
+	res.render("signup")
+}
+exports.login = async (req, res) => {
+	res.render("login")
+}
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const router	= express.Router()
 const indexController = require("./../controllers/indexController")
 
 router.get("/", indexController.home)
+router.get("/signup", indexController.signup)
+router.get("/login", indexController.login)
 
 module.exports = router
